ad8f53c2169dc55423aa41b5ab3cde5b1e67aaaf
Zane Schaffer <znschaffer@gmail.com>
2023-10-07 12:58:26 -0700

    add mock data

--- app/components/MGlobe.tsx
+++ app/components/MGlobe.tsx
@@ -1,8 +1,16 @@
 "use client"
 import Globe from "react-globe.gl"
+import MockData from "../data/mock"
 
 export default function MGlobe() {
+
+  let labelLabel = (d: any) => `
+        <div><b>${d.label}</b></div>
+        <div>${d.agency} - ${d.program} Program</div>
+        <div>Landing on <i>${new Date(d.date).toLocaleDateString()}</i></div>
+`
   return (
-    <Globe waitForGlobeReady={true} globeImageUrl={"./moon_map_small.jpeg"} showGraticules={true} />
+    <Globe waitForGlobeReady={true} labelText="label" labelSize={1.7}
+      labelLabel={labelLabel} labelsData={MockData} globeImageUrl={"./moon_map_small.jpeg"} showGraticules={true} />
   )
 }
