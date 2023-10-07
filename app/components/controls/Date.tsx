5a9a5376f6278adeb8bebe68b7531112bfc80b99
Xinrui Chen <xynree@gmail.com>
2023-10-07 12:57:59 -0700

    add date component

---
+++ app/components/controls/Date.tsx
@@ -0,0 +1,6 @@
+"use client"
+
+export default function MGlobe() {
+  const date = "1993"
+  return <div className="fixed z-10 bg-red-500 bottom-8 left-1/2">{date}</div>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState, useEffect } from "react"
 import MGlobe from "./components/MGlobe"
+import Date from "./components/controls/Date"
 
 export default function Home() {
   const [mounted, setMounted] = useState(false)
@@ -12,7 +13,7 @@ export default function Home() {
 
   return mounted ? (
     <div>
-      <div className="fixed z-10 w-1/2 h-12 bg-red-400 top-50vh left-12">hello</div>
+      <Date />
       <MGlobe />
     </div>
   ) : (
