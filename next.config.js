761d816ec55e1bb7e8cfa4a7284c7dd70d3c6b33
Vladimir Andral (Jr.) <andralvladimir@gmail.com>
2023-10-07 11:45:48 -0700

    Test

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/api/:path*'
             : '/api/',
+            // Hi
       },
     ]
   },
