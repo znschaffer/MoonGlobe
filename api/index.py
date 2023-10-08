c8d22d804d64b0658dd5782a9d3d7e304ed3ccc6
Riberiko <albericriko@gmail.com>
2023-10-08 09:10:43 -0700

    index.py data collection changed and moved csv files special space

--- api/index.py
+++ api/index.py
@@ -1,29 +1,60 @@
 from flask import Flask
-import csv
-import json
+import csv, json, os
+from astropy.time import Time
 app = Flask(__name__)
 
-levent_catalog = {'data':[]}
-_2003_Moonquake_Data = {'data':[]}
 
-def toJson(csvRelativePath, variable):
-    with open(csvRelativePath) as file:
+def jd_to_date(year, jd):
+    if year < 0:
+        year = 0
+    # Create a Time object for the specified JD
+    time = Time(jd, format='jd', scale='utc')
+
+    # Set the year component of the Time object
+    time = time.to_datetime()
+    time = time.replace(year=year)
+
+    # Extract the calendar date in ISO format
+    return time.isoformat()
+
+entries = os.scandir('./api/datasets')  #data source locations
+
+data_map = {} #storage for all file data
+
+#parses the discovered files adding them to the data map
+for entry in entries:
+    if(not entry.is_file() or not entry.name.endswith('.csv')): #only do csv entries
+        continue
+    
+    data = []
+    
+    with open(entry.path, "r") as file:
         csvReader = csv.DictReader(file)
 
         for row in csvReader:
-            variable['data'].append(row)
+            modified_row = {}
+            for key, value in row.items():  #loop for changing keys to lower case
+                new_key = key.lower()
+                if(new_key == 'long'):  #finds header of long and changes to lng
+                    new_key = 'lng'
+                elif(new_key == 'y'):
+                    new_key = 'year'
+                elif(new_key == 'day'):
+                    new_key = 'jd'
+                modified_row[new_key] = value
+
+            if 'date' in modified_row.keys():   #if date exists move on
+                continue
+
+            if('year' in modified_row.keys() and 'jd' in modified_row.keys()):
+                if(not modified_row['year']):
+                    print('aksdjflakdsjf')
+                    continue
+                date = jd_to_date(int(modified_row['year']), int(modified_row['jd']))
+
+            data.append(modified_row)
         
-        
+    data_map[entry.name.lower()[:-4]] = data
 
-
-
-@app.route("/api/levent")
-def levent():
-    return json.dumps(levent_catalog), 200, {'ContentType':'application/json'} 
-
-@app.route("/api/_2003_Moonquake_Data")
-def lognonne():
-    return json.dumps(_2003_Moonquake_Data), 200, {'ContentType':'application/json'} 
-
-toJson('api/levent.1008weber.csv', levent_catalog)
-toJson('api/_2003_Moonquake_Data.csv', _2003_Moonquake_Data)
+#@app.route('/api/sismic')
+print(data_map)
