237d16274775ae38d1977bc840978ab383214e5b
Zane Schaffer <znschaffer@gmail.com>
2023-10-08 01:51:29 -0700

    test" "

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      "/api/*": ["./public/*"],
+    },
+  },
   rewrites: async () => {
     return [
       {
-        source: '/api/:path*',
+        source: "/api/:path*",
         destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
-            // Hi
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/api/:path*"
+            : "/api/",
+        // Hi
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
